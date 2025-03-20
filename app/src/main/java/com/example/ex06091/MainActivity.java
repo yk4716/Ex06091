fef3d1488b1a7cb956d3a4c641dad4a3e21428be
yk4716 <yk4716@bs.amalnet.k12.il>
2025-03-20 09:40:52 +0200

    finished the program

--- app/src/main/java/com/example/ex06091/MainActivity.java
+++ app/src/main/java/com/example/ex06091/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex06091;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView wV;
+    String stringUrl;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wV = (WebView)findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        stringUrl = "https://berseva.iscool.co.il/default.aspx";
+        wV.loadUrl(stringUrl.toString());
+        wV.setWebViewClient(new MyWebViewClient());
+
+
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+            public boolean shouldOverideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+        }
+
     }
 }
